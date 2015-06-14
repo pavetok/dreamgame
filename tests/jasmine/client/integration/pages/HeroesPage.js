eb5d044021036c324bc200327a79c9419c01b0ea
pavetok <p.vetokhin@gmail.com>
2015-06-14 19:45:36 +0300

    fix element check

--- tests/jasmine/client/integration/pages/HeroesPage.js
+++ tests/jasmine/client/integration/pages/HeroesPage.js
@@ -1,14 +1,19 @@
 HeroesPage = function HeroesPage(url) {
-	this._url = url;
+	this.url = url;
 };
 
 HeroesPage.prototype.getUrl = function() {
-	return this._url;
+	return this.url;
 };
 
-HeroesPage.prototype.shouldContain = function(expected, done) {
-	done = done || function(){};
-	var heroName = $(".hero-name").text();
-	expect(heroName).toEqual(expected.name);
-	done();
+HeroesPage.prototype.shouldContain = function(expected) {
+	var $heroes = $(".hero");
+	expect($heroes.length).toEqual(1);
+
+	var $hero = $heroes.first();
+	expect($hero.is(":visible")).toBeTruthy();
+
+	var $heroName = $hero.find(".hero-name");
+	expect($heroName.text()).toEqual(expected.name);
+	expect($heroName.attr("href")).toEqual(expected.url);
 };
